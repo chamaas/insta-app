c4673723c70e48e28524be877117ba4838846870
chamaas <chamaas20@gmail.com>
2022-06-28 06:47:09 -0700

    add navigation

--- resources/views/client/hom.blade.php
+++ resources/views/client/hom.blade.php
@@ -1,38 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Instagram-Clone</title>
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-    />
-    <link rel="stylesheet" href="public/css/style.css" />
-    <link
-      rel="stylesheet"
-      href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-    />
-  </head>
-  <body>
+@extends('client_layout.client_layout')
+
+@section('title')
+Home
+@endsection
+
+@section('content')
+
     <!-- navigation -->
-    <nav class="navbar">
-      <div class="nav-wrapper">
-        <img class="brand-img" src="assets/images/logo.png" />
-        <form class="search-form">
-          <input type="text" class="search-box" placeholder="search.." />
-        </form>
-        <div class="nav-items">
-          <i class="icon fas fa-home"></i>
-          <i class="icon fas fa-plus"></i>
-          <i class="icon fas fa-heart"></i>
-          <div class="icon user-profile">
-            <i class="fas fa-user"></i>
-          </div>
-        </div>
-      </div>
-    </nav>
+    
 
     <!-- section -->
     <section class="main">
@@ -157,5 +132,4 @@
 
     <!-- script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
-  </body>
-</html>
+    @endsection
---
+++ resources/views/include/client_navbar.blade.php
@@ -0,0 +1,18 @@
+
+<nav class="navbar">
+    <div class="nav-wrapper">
+      <img class="brand-img" src="assets/images/logo.png" />
+      <form class="search-form">
+        <input type="text" class="search-box" placeholder="search.." />
+      </form>
+      <div class="nav-items">
+        <i class="icon fas fa-home"></i>
+        <i class="icon fas fa-plus"></i>
+        <i class="icon fas fa-heart"></i>
+        <div class="icon user-profile">
+          <i class="fas fa-user"></i>
+        </div>
+      </div>
+    </div>
+  </nav>
+ 
